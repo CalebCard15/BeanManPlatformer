6e9d196a220272f22c549bd4adc7d9c32d8d904b
Caleb Card <cjcard15@gmail.com>
2016-11-22 13:54:39 -0500

    Changed the battery sprite and effects
    Changed the old battery from a yellow circle to a battery shaped
    object. Also added a particle effect at the top and changed the script
    to make a sound when picked up

--- Assets/_Scripts/BatteryPickup.cs
+++ Assets/_Scripts/BatteryPickup.cs
@@ -5,8 +5,18 @@ public class BatteryPickup : MonoBehaviour {
 
 	public Score score;
 
+	private ParticleSystem particles;
+	private SpriteRenderer renderer;
+	private BoxCollider2D collider;
+	private AudioSource source;
+
+
 	// Use this for initialization
 	void Start () {
+		particles = GetComponentInChildren<ParticleSystem>();
+		renderer = GetComponent<SpriteRenderer>();
+		source = GetComponent<AudioSource>();
+		collider = GetComponent<BoxCollider2D>();
 		score = GameObject.Find("Score").GetComponent<Score>();
 	}
 	
@@ -14,8 +24,18 @@ public class BatteryPickup : MonoBehaviour {
 	{
 		if(col.tag == "Player")
 		{
-			score.score += 100;
-			Destroy(gameObject);
+			StartCoroutine(HandlePickup());
 		}
+	}
+
+	IEnumerator HandlePickup()
+	{
+		particles.gameObject.SetActive(false);
+		renderer.enabled = false;
+		collider.enabled = false;
+		score.score += 100;
+		source.Play();
+		yield return new WaitForSeconds(source.clip.length);
+		Destroy(gameObject);
 	}
 }
