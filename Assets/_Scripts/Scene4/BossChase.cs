c6efe1962c7374e50a06e760a2a57c4d4d122903
CalebCard15 <cjcard15@gmail.com>
2016-12-12 20:09:32 -0500

    Finished Scene4, fixed the other scenes UI systems

---
+++ Assets/_Scripts/Scene4/BossChase.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BossChase : MonoBehaviour {
+
+	public GameObject explosionEffect;
+
+	public GameObject massiveExplosion;
+	public GameObject outsideBackground;
+
+
+	public float moveSpeed = 5;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		print(col.tag); 
+		if(col.tag == "Player")
+		{
+			Instantiate(explosionEffect, col.transform.position, Quaternion.identity);
+			Destroy(col.gameObject);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+		}
+		else if(col.tag == "BossExit")
+		{
+			Instantiate(massiveExplosion, outsideBackground.transform.position, Quaternion.identity);
+			outsideBackground.SetActive(true);
+			moveSpeed = 30;
+		}
+		else if(col.tag == "ground")
+		{
+			//do nothing
+		}
+		else
+		{
+			Instantiate(explosionEffect, col.transform.position, Quaternion.identity);
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ Assets/_Scripts/Scene4/BossExit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BossExit : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		print("Get TRIGGERED");
+		if(col.tag == "Player")
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+		}
+			
+	}
+}
